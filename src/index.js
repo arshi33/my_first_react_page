d1b38de3bd14e5643c9afa0ebbd39a233563f217
arshi33 <arshifatema3@gmail.com>
2021-06-05 12:44:24 +0530

    react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
+import ReactDom from "react-dom";
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//always capital leetter for function name
+//always return something JSX(in function)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+    </section>
+  );
+}
+
+const Book = () => {
+  return ( 
+    <article className="book">
+     <Image/>
+     <Author/>
+     <Title></Title>
+    </article>
+  );
+};
+
+const Image = () => {
+  return( 
+    <div>
+      <img src='https://images-eu.ssl-images-amazon.com/images/I/61r2osjrwmL._AC_SX184_.jpg' alt='img1' />
+      </div>
+  );
+  
+};
+
+const Title = () => <h1>the waht found</h1>;
+const Author = () => <h2>annelia</h2>;
+
+
+ReactDom.render(<BookList/>, document.getElementById('root'));
+
+//JSX rules
+//return in single element like in only enclosed in one div
+//div/section/article/fragment
+//React.fragment can be used as well
+//use camelcase for html attribute
+//className in palce of class
+//close every element
+//formating
+
+
+//const Fun = ()=>{
+  //return React.createElement('h1',{},'hello world');
+//}
